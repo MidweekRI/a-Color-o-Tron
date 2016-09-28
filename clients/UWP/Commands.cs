526adbaf17c83f861955b32a514dd2398cc064a0
AndreyStelmakh <andrey.stelmakh@live.ru>
2016-09-28 22:23:48 +0300

    UWP client added; .gitignore added;

---
+++ clients/UWP/Commands.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueButton
+{
+    internal static class Device
+    {
+        static System.Net.Sockets.Socket _socket;
+        public static void Execute(IPEndPoint IPEndPoint, byte[] command)
+        {
+            _socket = new System.Net.Sockets.Socket(
+                System.Net.Sockets.AddressFamily.InterNetwork,
+                System.Net.Sockets.SocketType.Stream,
+                System.Net.Sockets.ProtocolType.Tcp);
+            var arg = new System.Net.Sockets.SocketAsyncEventArgs() { RemoteEndPoint = IPEndPoint };
+
+            arg.SetBuffer(command, 0, command.Length);
+            arg.Completed += Sending_Completed;
+
+            var result = _socket.ConnectAsync(arg);
+
+
+            //socket.SendToAsync(arg);
+
+        }
+
+        private static void Sending_Completed(object sender, System.Net.Sockets.SocketAsyncEventArgs e)
+        {
+            var arg = new System.Net.Sockets.SocketAsyncEventArgs() {   };
+            arg.Completed += Receiving_Completed;
+            arg.SetBuffer(new byte[1000], 0, 1000);
+            _socket.ReceiveAsync(arg);
+
+            //throw new NotImplementedException();
+
+        }
+
+        private static void Receiving_Completed(object sender, System.Net.Sockets.SocketAsyncEventArgs e)
+        {
+            _socket.Dispose();
+            _socket = null;
+
+        }
+
+        public static IEnumerable<byte> ToUtf8Bytes(this string src)
+        {
+            var result = new byte[0].AsEnumerable();
+
+            foreach (var ch in src.ToArray())
+            {
+                result = result.Concat(Encoding.Convert(Encoding.Unicode, Encoding.UTF8, BitConverter.GetBytes(ch)));
+
+            }
+
+            return result;
+
+        }
+
+    }
+
+    class InsCommand
+    {
+        public static Result Execute(IPEndPoint endPoint, Windows.UI.Xaml.Media.Imaging.BitmapImage bitmap)
+        {
+            string debug_value = "FF0FF0FF0FF0FF0F00F00F00F00F00FF0FF0FF0FF0FF000F00F00F00F00FF00F00F00F00F0000F00F00F00F00FF00F00F00F00F00F555F5F5555FF555F5F5555F";
+
+            byte[] new_line = new byte[] { 0xD, 0xA };
+            var cmd = (string.Format("INS ") + debug_value)
+                .ToUtf8Bytes()
+                .Concat(new_line)
+                .ToArray();
+
+            Device.Execute(endPoint, cmd);
+
+            return new Result();
+
+        }
+
+        public class Result { }
+
+    }
+
+    class DispCommand
+    {
+        public static Result Execute(IPEndPoint endPoint, string symbolName)
+        {
+            byte[] r = new byte[] { 0 };
+            var cmd1 = string.Format("disp {0}", symbolName)
+                .ToUtf8Bytes().ToArray();
+            var cmd = cmd1
+                .Concat(r)
+                .ToArray();
+
+            Device.Execute(endPoint, cmd);
+
+            return new Result();
+
+        }
+
+        public class Result { }
+
+    }
+
+    class RetrCommand
+    {
+    }
+
+
+
+}
---
+++ clients/UWP/MainPage.xaml.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BlueButton
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public string IpAddress { get; set; }
+        public int Port { get; set; }
+        private ObservableCollection<ButtonContent> _PivotItem1Content = new ObservableCollection<ButtonContent>();
+        private ObservableCollection<ButtonContent> _PivotItem2Content = new ObservableCollection<ButtonContent>();
+        private ObservableCollection<ButtonContent> _PivotItem3Content = new ObservableCollection<ButtonContent>();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = this;
+
+            IpAddress = "192.168.1.107";
+            Port = 2121;
+
+            PivotItem1Content.Add(new ButtonContent() { Name = "зайчик" });
+            PivotItem1Content.Add(new ButtonContent() { Name = "слоник" });
+            PivotItem1Content.Add(new ButtonContent() { Name = "рыба" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "дерево" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "куст" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "молния" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "небо" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "лес" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "волны" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "огонь" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "цветы" });
+            PivotItem2Content.Add(new ButtonContent() { Name = "ветер" });
+            PivotItem3Content.Add(new ButtonContent() { Name = "circle" });
+            PivotItem3Content.Add(new ButtonContent() { Name = "square" });
+            PivotItem3Content.Add(new ButtonContent() { Name = "треугольник" });
+            PivotItem3Content.Add(new ButtonContent() { Name = "line" });
+
+        }
+
+        public ObservableCollection<ButtonContent> PivotItem1Content
+        {
+            get
+            {
+                return _PivotItem1Content;
+            }
+
+            set
+            {
+                _PivotItem1Content = value;
+            }
+        }
+
+        public ObservableCollection<ButtonContent> PivotItem2Content
+        {
+            get
+            {
+                return _PivotItem2Content;
+            }
+
+            set
+            {
+                _PivotItem2Content = value;
+            }
+        }
+
+        public ObservableCollection<ButtonContent> PivotItem3Content
+        {
+            get
+            {
+                return _PivotItem3Content;
+            }
+
+            set
+            {
+                _PivotItem3Content = value;
+            }
+        }
+
+        public class ButtonContent : INotifyPropertyChanged
+        {
+            private Windows.UI.Xaml.Media.Imaging.BitmapImage _Image;
+
+            public Windows.UI.Xaml.Media.Imaging.BitmapImage Image
+            {
+                get
+                {
+                    return _Image;
+                }
+
+                set
+                {
+                    _Image = value;
+
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Image"));
+
+                    }
+
+                }
+
+            }
+
+            public string Name
+            {
+                get
+                {
+                    return _Name;
+
+                }
+
+                set
+                {
+                    _Name = value;
+
+                    if(PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+
+                    }
+
+                }
+
+            }
+
+            private string _Name;
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var button = e.OriginalSource as Button;
+
+            if(button != null)
+            {
+                var dataContext = button.DataContext as ButtonContent;
+
+                if (dataContext != null)
+                {
+                    //var r = this.Resources["_img1"] as Bitmap;
+                    //ImageSource j;
+                    //var y = new Windows.UI.Xaml.Media.Imaging.WriteableBitmap(5, 8) ;//..SetSource()
+                    //y.SetSourceAsync()
+                    //y.
+                    //foreach(var pixel in y.PixelBuffer.ToArray())
+                    //{
+
+
+                    //}
+                    ////var str = dataContext.Image.ToString();
+                    //var str = new Windows.UI.Xaml.Media.Imaging.WriteableBitmap(5, 8).PixelBuffer.ToArray();
+
+                    InsCommand.Execute(
+                        new System.Net.IPEndPoint(System.Net.IPAddress.Parse(IpAddress), Port),
+                        null);
+
+                }
+
+            }
+
+        }
+
+    }
+
+}
